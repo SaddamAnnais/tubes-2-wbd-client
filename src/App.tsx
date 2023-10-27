09761c3b9ac59af416006ae7fd380b3c7284124a
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-10-27 10:31:01 +0700

    feat: docker setup

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button
+          onClick={() => {
+            return setCount((count) => {
+              return count + 1;
+            });
+          }}
+        >
+          count is {count}
+        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
