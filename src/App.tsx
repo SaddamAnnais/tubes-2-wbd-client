1dc63455911c7cdee10bb2908751f275b9afd60c
ashnchiquita <16521248@mahasiswa.itb.ac.id>
2023-11-15 06:51:12 +0700

    feat: init routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,44 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Outlet, createBrowserRouter } from 'react-router-dom';
+import APIProvider from './providers/ApiProvider';
+import AuthProvider from './providers/AuthProvider';
+import { Login, Register, Protected } from './pages';
+import { Layout } from './layouts';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const Providers = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            return setCount((count) => {
-              return count + 1;
-            });
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <APIProvider>
+      <AuthProvider>
+        <Outlet />
+      </AuthProvider>
+    </APIProvider>
   );
-}
+};
+const Router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Providers />,
+    children: [
+      {
+        path: '/',
+        element: <Layout />,
+        children: [
+          {
+            path: '/protected',
+            element: <Protected />,
+          },
+        ],
+      },
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/register',
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
-export default App;
+export default Router;
