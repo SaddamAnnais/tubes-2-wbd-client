ee1ed3af7ae6687cc8ffdc1a8c0222056d3b2bce
SaddamAnnais <saddamannaiss@gmail.com>
2023-11-16 08:48:40 +0700

    feat: add collection modals

--- src/pages/collection/page.tsx
+++ src/pages/collection/page.tsx
@@ -1,83 +1,35 @@
 import { Button } from '@/components/ui/button';
-import { Plus, X } from 'lucide-react';
+import { Plus } from 'lucide-react';
 import CollectionCard from './CollectionCard';
-import { MouseEvent, useState } from 'react';
+import { useState } from 'react';
+import AddCollectionModals from './AddCollectionModals';
 
 const Collection = () => {
-  const [addCollectionModals, setAddCollectionModals] = useState(false);
+  const [isModalsOpen, setIsModalsOpen] = useState(false);
 
-  const closeCollectionModals = (event: MouseEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    if (event.target === event.currentTarget) {
-      setAddCollectionModals(false);
-    }
-  };
+  // TO BE DELETED
+  const componentArray = new Array(12).fill(null);
 
   return (
-    <main className={`w-full h-full pt-28 py-8 px-20 ${addCollectionModals ? 'overflow-hidden max-h-screen' : ''} `}>
-      <div
-        className={`w-screen h-screen fixed z-50 flex justify-center items-center bg-primary/50 top-0 left-0 ${
-          addCollectionModals ? '' : 'hidden'
-        }`}
-        onClick={(e) => {
-          closeCollectionModals(e);
-        }}
-      >
-        <div className="w-1/2 h-1/2 bg-primary-foreground rounded-lg">
-          <div className="flex flex-row justify-between px-6 py-4">
-            <p className="text-2xl font-semibold">Add New Playlist</p>
-            <Button
-              size="icon"
-              onClick={() => {
-                setAddCollectionModals(false);
-              }}
-            >
-              <X />
-            </Button>
-          </div>
-        </div>
-      </div>
+    <main className={`w-full h-full pt-28 py-8 px-20 ${isModalsOpen ? 'fixed' : ''} `}>
+      <AddCollectionModals isModalsOpen={isModalsOpen} setIsModalsOpen={setIsModalsOpen} />
       <header className="flex flex-row justify-between mb-8">
         <h1 className="font-bold text-5xl">My Collection</h1>
         <Button
           className="text-lg"
           onClick={() => {
-            setAddCollectionModals(true);
+            setIsModalsOpen(true);
           }}
         >
           <Plus className="mr-2" /> New
         </Button>
       </header>
       <div className="w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+
         {/* iterate  */}
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
-        <CollectionCard title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />
+        {componentArray.map((_, index) => {
+          return <CollectionCard key={index} title="test title" total_recipe={10} created_at={new Date()} collection_id={1} />;
+        })}
       </div>
     </main>
   );
