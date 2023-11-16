6144ed99a21a6d7415478c1810186c28e49bab96
fakihap <fakih.anugerah@gmail.com>
2023-11-16 12:23:09 +0700

    note: this component is not in browser router, hence using manual a href method
    note: this component is not in browser router, hence using manual a href method

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -3,11 +3,11 @@ import logo_src from '/logo-with-bg.png';
 
 const Logo = () => {
   return (
-    <div className="w-[10rem] flex flex-row justify-between items-center">
+    <a href='/' className="w-[10rem] flex flex-row justify-between items-center">
       <img className="h-full aspect-square" src={logo_src} alt="logo" />
       <span className="pl-4 text-xl font-semibold">Cooklyst</span>
       <span className="text-2xl font-light">PRO</span>
-    </div>
+    </a>
   );
 };
 
