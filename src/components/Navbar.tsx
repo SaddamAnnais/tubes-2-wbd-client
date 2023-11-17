03dcb6841054a1692c44664f856381a416b35e88
fakihap <fakih.anugerah@gmail.com>
2023-11-17 08:31:07 +0700

    fix: navbar conditional now depends on user logged in rather than token

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,17 @@ import { Separator } from './ui/separator.tsx';
 import { Button } from './ui/button.tsx';
 import { Link } from 'react-router-dom';
 
-import { useAPI } from '@/contexts/api-context.ts';
+import { useAuth } from '@/contexts/auth-context.ts';
 
 const Navbar = () => {
-  const {token} = useAPI();
+  const { user } = useAuth();
 
   return (
     <div className="fixed top-0 bg-white w-full h-24 flex flex-col">
       <div className="w-full h-full py-8 px-16 flex flex-row justify-between">
         <Logo />
         { 
-          token && 
+          user && 
           <div className='w-1/4 h-full flex  justify-between items-center'>
             <Button variant="outline" className='hover:text-white hover:bg-primary transition-all duration-300'>
               <Link to="/recipe/add">New Recipe</Link>
